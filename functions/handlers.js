0f63005ee4c7911ed9a1a9dae147d8eb228f9789
Q3nfQ <bender1423@hotmail.com>
2024-06-14 00:30:38 +0300

    Update handlers.js

--- functions/handlers.js
+++ functions/handlers.js
@@ -1,6 +1,6 @@
 const status = "AUTOMATIC";
 const botName = "🌇 MULTI MUSIX ";
-const rtxText = "GlaceYT";
+const rtxText = "bad boys";
 const version = "Latest@ v1.7"; 
 const startTime = Date.now();
 
