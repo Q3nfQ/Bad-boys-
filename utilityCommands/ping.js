ed406afcfa13ece164498ecf956a03993ad617c3
Q3nfQ <bender1423@hotmail.com>
2024-06-14 01:26:18 +0300

    Update ping.js

--- utilityCommands/ping.js
+++ utilityCommands/ping.js
@@ -1,19 +1,29 @@
-const { EmbedBuilder } = require('discord.js');
+const { EmbedBuilder } = require('discord.js')
 
 module.exports = {
-    name: 'ping',
-    description: 'Check the bot\'s ping',
-    execute(message, args) {
-        const ping = message.client.ws.ping;
+  name: "ping",
+  description: "check the bot latency",
+  permissions: "0x0000000000000800",
+  options: [],
+  run: async (client, interaction) => {
+
+
+    try {
+
+      const start = Date.now();
+      interaction.reply("Pinging....").then(msg => {
+        const end = Date.now();
         const embed = new EmbedBuilder()
-            .setColor('#FFFFFF')
-            .setTitle('Bot Latency')
-            .setDescription(`📊 The bot's ping is ${ping}ms.`)
-            .setTimestamp();
+          .setColor(`#6190ff`)
+          .setTitle(`Bot Latency`)
+          .setDescription(`**Pong** : ${end - start}ms`)
+        return interaction.editReply({ embeds: [embed] }).catch(e => { });
+      }).catch(err => { })
 
-
-        message.reply({ embeds: [embed] });
-    },
+    } catch (e) {
+    console.error(e); 
+  }
+  },
 };
 
 /*
